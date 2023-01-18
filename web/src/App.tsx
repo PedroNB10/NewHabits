dd75b8f9606967f4f81c6c6077f2230fe8da01e8
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2023-01-17 21:57:22 -0300

    setup do front pronto

---
+++ web/src/App.tsx
@@ -0,0 +1,24 @@
+import './styles/global.css'
+
+import { Habit } from './components/Habit'
+
+function App() {
+
+
+  return (
+   <div>
+      <Habit completed={3} />
+      <Habit completed={4} />
+      <Habit completed={5} />
+      <Habit completed={6} />
+      <Habit completed={7} />
+   </div>
+     
+    
+  )
+}
+
+export default App
+
+// Componente: Reaproveitar / isolar
+// Propriedade: informação enviada para modificar um componente visual ou comportamentalmente
