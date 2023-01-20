6e69baf7b19ba6985e79ac87d2573144514fa191
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2023-01-19 22:00:01 -0300

    dias da semana contados funcionando

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,26 @@
 import './styles/global.css'
+import { Header } from './components/Header'
+import { SummaryTable } from './components/Summarytable'
 
-import { Habit } from './components/Habit'
+//import { Habit } from './components/Habit'
 
-function App() {
+export function App() {
 
 
   return (
-   <div>
-      <Habit completed={3} />
-      <Habit completed={4} />
-      <Habit completed={5} />
-      <Habit completed={6} />
-      <Habit completed={7} />
+   <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+          <Header/>
+          <SummaryTable/>
+      </div>
+    
    </div>
      
     
   )
 }
 
-export default App
+
 
 // Componente: Reaproveitar / isolar
 // Propriedade: informação enviada para modificar um componente visual ou comportamentalmente
