ea6aca045a819b42855653e3c16e8424126c107e
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2023-01-21 17:48:11 -0300

    animações e transições prontas

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -10,13 +10,13 @@ import { NewHabitForm } from './NewHabitsForm';
 export function Header() {
 
   return (
-    <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
+    <div className='w-full max-w-3xl mx-auto flex items-center justify-between '>
       <img src={logoImage} alt="Habits" />
 
       <Dialog.Root>
         <Dialog.Trigger
           type='button'
-          className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'
+          className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300 transition-colors focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 focus:ring-offset-background'
         >
           <Plus size={20} className="text-violet-500 " />
           Novo hábito
--- web/src/components/NewHabitsForm.tsx
+++ web/src/components/NewHabitsForm.tsx
@@ -62,7 +62,7 @@ export function NewHabitForm() {
             <input type="text"
                 id="title"
                 placeholder="ex.: Exercitar, Dormir bem, etc..."
-                className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+                className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 group-focus:ring-2 focus:ring-violet-600 focus:offset-2 focus:ring-offset-zinc-900"
                 autoFocus
                 value={title}
                 onChange={event => setTitle(event.target.value) }
@@ -71,19 +71,19 @@ export function NewHabitForm() {
                 Qual a recorrência?
             </label>
 
-            <div className="flex flex-col gap-2 mt-3">
+            <div className="flex flex-col gap-2 mt-3 ">
                 {availableWeekDays.map((weekDay,index) => {
                     return (
                         <Checkbox.Root
                             key={weekDay}
-                            className='flex items-center gap-3 group'
+                            className='flex items-center gap-3 group focus:outline-none'
                             checked={weekDays.includes(index)}
                             onCheckedChange={() =>{ 
                                 handleToggleWeekDay(index)
                             }}
                         >
                             <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500
-                            group-data-[state=checked]:border-green-500'>
+                            group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-green-600 group-focus:ring-offset-2  group-focus:ring-offset-background'>
                                 <Checkbox.Indicator>
                                     <Check size={20} className="text-white" />
                                 </Checkbox.Indicator>
@@ -99,7 +99,9 @@ export function NewHabitForm() {
             </div>
 
 
-            <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500" >
+            <button 
+            type="submit" 
+            className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:offset-2 focus:ring-offset-zinc-900" >
                 <Check size={20} weight="bold" />
                 Confirmar
             </button>
