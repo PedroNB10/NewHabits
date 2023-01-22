6e57c28c1578ea65523e295c0a43c66754b1d44a
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2023-01-22 17:27:41 -0300

    aplicação pronta

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,25 +1,18 @@
 import Fastify from 'fastify'
-import cors from "@fastify/cors"
-import { appRoutes } from './lib/routes'
+import cors from '@fastify/cors'
+import { appRoutes } from './routes'
+
 
 const app = Fastify()
 
-/*
-GET: Buscar informação
-POST: Fazer uma publicação de uma informação
-PUT: Atualizar um recurso por completo
-PATCH: Atualizar uma informação específica de um recurso
-DELETE: Deletar
-
-*/
 
 app.register(cors)
 app.register(appRoutes)
 
 
 app.listen({
-    port:3333,
+  port: 3333,
 }).then(() => {
-    console.log('HTTP Server running')
+  console.log('HTTP Server running!')
 })
 
