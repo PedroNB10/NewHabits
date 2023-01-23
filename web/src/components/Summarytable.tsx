831854a4e76c025d2b7005a39693241c3508f041
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2023-01-23 12:00:37 -0300

    ajuste paleta de cores

--- web/src/components/Summarytable.tsx
+++ web/src/components/Summarytable.tsx
@@ -70,7 +70,7 @@ export function SummaryTable() {
 
         {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
           return (
-            <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
+            <div key={i} className="w-10 h-10 bg-blue-200 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
           )
         })}
       </div>
